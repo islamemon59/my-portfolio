06bb854d2c46f5d0940d8207b369088172e9159b
islamemon59 <islamemon59@gmail.com>
2025-06-30 13:05:01 +0600

    some bug fixes

--- src/Hooks/useActiveSectionTitle.jsx
+++ src/Hooks/useActiveSectionTitle.jsx
@@ -13,8 +13,8 @@ export default function useActiveSectionTitle() {
       else if (scrollY >= 600 && scrollY < 1400) setActiveSection("About");
       else if (scrollY >= 1400 && scrollY < 1800) setActiveSection("Skills");
       else if (scrollY >= 1800 && scrollY < 3000) setActiveSection("Education");
-      else if (scrollY >= 3000 && scrollY <3200) setActiveSection("Projects");
-      else if (scrollY >= 3200 && scrollY <3600) setActiveSection("Contact");
+      else if (scrollY >= 3000 && scrollY <3400) setActiveSection("Projects");
+      else if (scrollY >= 3400 && scrollY <3600) setActiveSection("Contact");
     };
 
     window.addEventListener("scroll", handleScroll);
